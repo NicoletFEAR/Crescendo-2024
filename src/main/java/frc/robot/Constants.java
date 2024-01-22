117598a01c9ac71572683d6f2488542c70108495
Akaswan <akaswan06@gmail.com>
2024-01-21 21:33:15 -0600

    Got rid of Drive Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,14 +6,8 @@ package frc.robot;
 
 import java.util.TreeMap;
 
-import com.pathplanner.lib.util.PIDConstants;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
 import frc.robot.subsystems.launcher.LauncherFlywheel.LauncherFlywheelState;
 import frc.robot.subsystems.launcher.LauncherWrist.LauncherWristState;
@@ -22,7 +16,6 @@ import frc.robot.subsystems.templates.PositionSubsystem.PositionSubsystemType;
 import frc.robot.subsystems.templates.SubsystemConstants.PositionSubsystemConstants;
 import frc.robot.subsystems.templates.SubsystemConstants.SparkMaxConstants;
 import frc.robot.subsystems.templates.SubsystemConstants.VelocitySubsystemConstants;
-import frc.lib.utilities.SwerveModuleConstants;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -48,134 +41,6 @@ public final class Constants {
   public static final class OperatorConstants {
     public static final int kDriverControllerPort = 0;
     public static final int kOperatorControllerPort = 0;
-  }
-
-  public static final class AutoConstants {
-    public static Translation2d[] kNotePlacements = new Translation2d[] {
-      new Translation2d(7.68, 7.44),
-      new Translation2d(7.68, 5.78),
-      new Translation2d(7.68, 4.10),
-      new Translation2d(7.68, 2.44),
-      new Translation2d(7.68, 0.78)
-    };
-  }
-
-  public static final class DriveConstants {
-
-    public static double drivekp = 0.15751;
-    public static double driveki = 0.0;
-    public static double drivekd = 0.0;
-    public static double drivekff = 0.23983;
-    public static double driverampRate = 1.0;
-
-    public static double turnkp = 0.1;
-    public static double turnki = 0.0;
-    public static double turnkd = 0.0;
-    public static double turnkff = 0.0;
-
-    public static double kDriveModifier = 2;
-    public static double kTurnModifier = 2;
-
-    /* The lower this is the more you want odometry to trust the april tags
-    Scales based on the percentage of an april tag in view
-    Dont do anything below 0*/
-    public static double kAprilTagTrustMultiplier = 1.0;
-
-    public static int kPigeon = 0;
-
-    public static final double kTrackWidth =
-        Units.inchesToMeters(20.67); // Distance between centers of right and left wheels on robot
-
-    public static final double kWheelBase =
-        Units.inchesToMeters(20.76); // Distance between centers of front and back wheels on robot
-
-    public static final double kDriveBaseRadius =
-        Math.hypot(
-            Units.inchesToMeters(32) / 2,
-            Units.inchesToMeters(32)
-                / 2); // Distance from center of the robot to corner of the bumpers
-
-    public static final double kMaxMetersPerSecond =
-        Units.feetToMeters(12); // Run drivebase at max speed on the ground to find top speed
-
-    public static final double kDriveGearRatio = 8.14; // MK4i L1 Neo, find on sds website
-
-    public static final double kTurnGearRatio =
-        150.0 / 7.0; // MK4i turning ratio MK4i Neo, find on sds website
-
-    public static final double kWheelDiameter = Units.inchesToMeters(3.79); // Wheel diameter
-
-    public static final double kMaxRotationRadiansPerSecond =
-        Math.PI * 2.0; // Just kind of find what works, this is from 930 2023
-
-    public static final double kRegularSpeed = 1; // Regular speed multiplier of robot
-
-    public static final double kSlowSpeed = 0.4; // Slow speed multiplier of robot
-
-    public static final int kFrontLeftDriveMotor = 12;
-    public static final int kFrontLeftSteerMotor = 11;
-    public static final int kFrontLeftSteerEncoder = 1;
-    public static final double kFrontLeftOffset = 52.03;
-    public static final SwerveModuleConstants kFrontLeft =
-        new SwerveModuleConstants(
-            kFrontLeftDriveMotor, kFrontLeftSteerMotor, kFrontLeftSteerEncoder, kFrontLeftOffset);
-
-    public static final int kFrontRightDriveMotor = 18;
-    public static final int kFrontRightSteerMotor = 17;
-    public static final int kFrontRightSteerEncoder = 7;
-    public static final double kFrontRightSteerOffset = 144.22;
-    public static final SwerveModuleConstants kFrontRight =
-        new SwerveModuleConstants(
-            kFrontRightDriveMotor,
-            kFrontRightSteerMotor,
-            kFrontRightSteerEncoder,
-            kFrontRightSteerOffset);
-
-    public static final int kBackLeftDriveMotor = 14;
-    public static final int kBackLeftSteerMotor = 13;
-    public static final int kBackLeftSteerEncoder = 3;
-    public static final double kBackLeftSteerOffset = 45.25;
-    public static final SwerveModuleConstants kBackLeft =
-        new SwerveModuleConstants(
-            kBackLeftDriveMotor, kBackLeftSteerMotor, kBackLeftSteerEncoder, kBackLeftSteerOffset);
-
-    public static final int kBackRightDriveMotor = 16;
-    public static final int kBackRightSteerMotor = 15;
-    public static final int kBackRightSteerEncoder = 5;
-    public static final double kBackRightSteerOffset = 231.1;
-    public static final SwerveModuleConstants kBackRight =
-        new SwerveModuleConstants(
-            kBackRightDriveMotor,
-            kBackRightSteerMotor,
-            kBackRightSteerEncoder,
-            kBackRightSteerOffset);
-
-    public static final double kDriveRevToMeters = ((kWheelDiameter * Math.PI) / kDriveGearRatio);
-    public static final double kDriveRpmToMetersPerSecond = kDriveRevToMeters / 60.0;
-    public static final double kTurnRotationsToDegrees = 360.0 / kTurnGearRatio;
-
-    public static final Translation2d[] kModuleTranslations = {
-      new Translation2d(kWheelBase / 2, kTrackWidth / 2),
-      new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
-      new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
-      new Translation2d(-kWheelBase / 2, -kTrackWidth / 2)
-    };
-
-    public static final SwerveDriveKinematics kDriveKinematics =
-        new SwerveDriveKinematics(kModuleTranslations);
-
-    public static final PIDConstants kPathPlannerTranslationPID = new PIDConstants(5.0, 0, 0);
-    public static final PIDConstants kPathPlannerRotationPID = new PIDConstants(5.0, 0, 0);
-
-    public static final SwerveModuleState[] kXWheels = {
-      new SwerveModuleState(0, Rotation2d.fromDegrees(45)),
-      new SwerveModuleState(0, Rotation2d.fromDegrees(-45)),
-      new SwerveModuleState(0, Rotation2d.fromDegrees(135)),
-      new SwerveModuleState(0, Rotation2d.fromDegrees(-135)),
-    };
-
-    public static final Translation2d kBlueSpeakerPosition = new Translation2d(0, 5.56);
-    public static final Translation2d kRedSpeakerPosition = new Translation2d(0, 2.65);
   }
 
   public static final class LauncherConstants {
