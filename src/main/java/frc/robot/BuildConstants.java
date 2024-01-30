b803a974e5018bc1db7ecb94337ddbce38725ac9
zee <93955867+PrestonBohnsack@users.noreply.github.com>
2024-01-29 18:17:05 -0600

    github version update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "110e8a08347f479cf6d45b8d14fca4ea7ced26ee";
-  public static final String GIT_DATE = "2024-01-27 17:31:12 EST";
-  public static final String GIT_BRANCH = "Launcher-State-Machine";
-  public static final String BUILD_DATE = "2024-01-27 19:02:47 EST";
-  public static final long BUILD_UNIX_TIME = 1706400167963L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "241a02c792c38143c81a060c13f7a65452eacf6d";
+  public static final String GIT_DATE = "2024-01-27 19:04:30 EST";
+  public static final String GIT_BRANCH = "Intake-State-Machine";
+  public static final String BUILD_DATE = "2024-01-27 19:26:10 EST";
+  public static final long BUILD_UNIX_TIME = 1706401570373L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
