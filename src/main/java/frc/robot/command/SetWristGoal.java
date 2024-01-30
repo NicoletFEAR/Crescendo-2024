25fb51b9607c80d3158d033ec1d8bfab429a948c
PrestonBohnsack <93955867+PrestonBohnsack@users.noreply.github.com>
2024-01-30 07:57:49 -0600

    Create SetWristGoal.java

---
+++ src/main/java/frc/robot/command/SetWristGoal.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Intake;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class SetWristGoalToTrap extends InstantCommand {
+
+  public Intake m_Intake;
+  double setPoint
+
+  public SetWristGoal(Intake newIntake, double newSetPoint) {
+    m_Intake = newIntake;
+    setPoint = newSetPoint;
+
+    addRequirements(newIntake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_Intake.setWristSetPoint(setPoint);
+  }
+}
