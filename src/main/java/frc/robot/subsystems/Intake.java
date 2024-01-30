0abc7c5b3d2033cd5c306d7cc9d1e5ec64f0ce6b
PrestonBohnsack <93955867+PrestonBohnsack@users.noreply.github.com>
2024-01-30 07:54:56 -0600

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -12,7 +12,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -21,17 +21,41 @@ public class Intake extends SubsystemBase {
   final CANSparkMax intakeRollerMotor;
   final CANSparkMax intakeWristMotor;
   final RelativeEncoder intakeWristEndcoder;
+  final PIDController intakeWristPID;
+  final double intakeWristMaxSetpoint = 10;
+  final double intakeWristMinSetpoint = 0;
+  final double intakeWristFloorSetpoint = 1;
+  final double intakeWristAmpSetpoint = 3;
+  final double intakeWristInSetpoint = 8;  
+  double intakeWristVariableSetpoint = 5;
+  final DigitalInput frontBeamBreak;
+  final DigitalInput backBeamBreak;
 
   /** Creates a new Intake. */
   public Intake() {
     intakeRollerMotor = new CANSparkMax(Constants.intakeRollerMotorID, MotorType.kBrushless);
     intakeWristMotor = new CANSparkMax(Constants.intakeWristMotorID, MotorType.kBrushless);
     intakeWristEndcoder = intakeWristMotor.getEncoder();
+    
+    intakeWristPID = new PIDController(0.1, 0, 0);
+    intakeWristPID.setTolerance(0.5);
+    SmartDashboard.putNumber("Intake Wrist Kp", intakeWristPID.getP());
+    SmartDashboard.putNumber("Intake Wrist Ki", intakeWristPID.getI());
+    SmartDashboard.putNumber("Intake Wrist Kd", intakeWristPID.getD());
+    SmartDashboard.putNumber("Intake Wrist Variable Set Point", intakeWristVariableSetpoint);
+
+    frontBeamBreak = new DigitalInput(0);
+    backBeamBreak = new DigitalInput(1);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    intakeWristPID.setP(SmartDashboard.getNumber("Intake Wrist Kp", PIDController.getP()));
+    intakeWristPID.setI(SmartDashboard.getNumber("Intake Wrist Ki", PIDController.getI()));
+    intakeWristPID.setD(SmartDashboard.getNumber("Intake Wrist Kd", PIDController.getD()));
+    intakeWristVariableSetpoint = SmartDashboard.getNumber("Intake Wrist Variable Set Point", intakeWristVariableSetpoint);
+    SmartDashboard.putBoolean("Front Beam Break", frontBeamBreak.get());
+    SmartDashboard.putBoolean("Back Beam Break", backBeamBreak.get());
   }
 
   public void driveRoller(double speed){
@@ -40,6 +64,26 @@ public class Intake extends SubsystemBase {
 
   public void driveWrist(double speed){
     intakeWristMotor.set(speed);
+  }
+
+  public boolean getFrontBeamBreak(){
+    return frontBeamBreak.get();
+  }
+
+  public boolean getBackBeamBreak(){
+    return backBeamBreak.get();
+  }
+
+  public void setWristSetpoint(double position){
+    intakeWristPID.setSetpoint(position);
+  }
+
+  public double getWristSetpoint(){
+    return intakeWristPID.getSetpoint();
+  }
+
+  public double getWristPosition(){
+    return intakeWristEncoder.getPosition();
   }
 
   public void setWristZero(){
