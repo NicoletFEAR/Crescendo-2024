6cff372848160961845e1632c567a37435a7c5b6
Akaswan <akaswan06@gmail.com>
2024-02-04 19:37:12 -0600

    Set up

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,36 +4,37 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.commands.ControlMotor;
-import frc.robot.commands.SetMotorSpeed;
-import frc.robot.subsystems.MotorFEAR;
 
 public class RobotContainer {
 
-  public static CommandXboxController m_driverController = new CommandXboxController(0);
-  public static MotorFEAR m_flex;
+  private CANSparkMax motor1 = new CANSparkMax(0, MotorType.kBrushless);
+  private CANSparkMax motor2 = new CANSparkMax(1, MotorType.kBrushless);
+
+  double voltage = 0;
 
   public RobotContainer() {
-    m_flex = new MotorFEAR();
+
+    SmartDashboard.putNumber("Motor Voltage", 0);
 
     configureBindings();
   }
 
-  private void configureBindings() {
-    m_flex.setDefaultCommand(new ControlMotor(m_flex));
-    m_driverController.b().onTrue(new SetMotorSpeed(m_flex, 1));
-    m_driverController.x().onTrue(new SetMotorSpeed(m_flex, -1));
-    m_driverController.a().onTrue(new SetMotorSpeed(m_flex, 0));
-  }
-
-  public static CommandXboxController getController(){
-    return m_driverController;
-  }
+  private void configureBindings() {}
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
+  }
+
+  public void periodic() {
+    voltage = SmartDashboard.getNumber("Motor Voltage", 0);
+
+    motor1.setVoltage(voltage);
+    motor2.setVoltage(-voltage);
   }
 }
