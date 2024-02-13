01766723dcf9ad4161f3cd515613a051dac3b632
Akaswan <akaswan06@gmail.com>
2024-02-13 15:39:15 -0600

    Updated libs

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 63;
-  public static final String GIT_SHA = "17768b9558ea444c21199f2a3939e7e3d01789df";
-  public static final String GIT_DATE = "2024-02-12 21:52:05 EST";
+  public static final int GIT_REVISION = 65;
+  public static final String GIT_SHA = "56da71ff1e38ac7e314ff02da0533e56df7471f0";
+  public static final String GIT_DATE = "2024-02-12 23:11:10 EST";
   public static final String GIT_BRANCH = "Reveal05";
-  public static final String BUILD_DATE = "2024-02-12 22:58:10 EST";
-  public static final long BUILD_UNIX_TIME = 1707796690334L;
+  public static final String BUILD_DATE = "2024-02-13 16:38:32 EST";
+  public static final long BUILD_UNIX_TIME = 1707860312788L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
