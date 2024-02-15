c4e93ac6abadef512f948a92f1aa9063e0463d33
Akaswan <akaswan06@gmail.com>
2024-02-14 22:13:27 -0600

    Changed Autos

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -95,9 +95,9 @@ public final class Constants {
     public static final double kMaxRotationRadiansPerSecond =
         Math.PI * 2.0; // Just kind of find what works, this is from 930 2023
 
-    public static final double kRegularSpeed = 1; // Regular speed multiplier of robot
+    public static final double kRegularSpeed = .6; // Regular speed multiplier of robot
 
-    public static final double kSprintSpeed = .5; // Slow speed multiplier of robot
+    public static final double kSprintSpeed = 1; // Slow speed multiplier of robot
 
     public static final int kFrontLeftDriveMotor = 12;
     public static final int kFrontLeftSteerMotor = 11;
