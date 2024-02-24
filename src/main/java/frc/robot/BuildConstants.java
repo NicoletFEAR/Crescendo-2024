7b2ba2f13439d77a1cc4bad5f1cb09a2e4f57e84
PrestonBohnsack <150401208+polinga@users.noreply.github.com>
2024-02-24 13:40:51 -0600

    updated wpilib vscode

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 104;
-  public static final String GIT_SHA = "6967c5ecb66c7cd8ff7cd4ee8a3d86512475169a";
-  public static final String GIT_DATE = "2024-02-23 20:19:55 EST";
+  public static final int GIT_REVISION = 109;
+  public static final String GIT_SHA = "11027eec2d1cf11e8aae6c24d49e5109a0efb86e";
+  public static final String GIT_DATE = "2024-02-24 10:22:23 EST";
   public static final String GIT_BRANCH = "Duluth-03-Optimization";
-  public static final String BUILD_DATE = "2024-02-23 22:43:55 EST";
-  public static final long BUILD_UNIX_TIME = 1708746235058L;
+  public static final String BUILD_DATE = "2024-02-24 14:40:42 EST";
+  public static final long BUILD_UNIX_TIME = 1708803642092L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
